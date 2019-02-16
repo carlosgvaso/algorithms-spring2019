54ce4ce9659dd514667cd3baa8d169bd2c1185cc
Jose C.M. Garcia-Vaso <carlosgvaso@gmail.com>
2019-02-16 11:02:41 -0600

    Added skeleton class.

---
+++ src/hw1/InSet.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package hw1;
+
+/**
+ * @author jcarlos
+ *
+ */
+public class InSet {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
